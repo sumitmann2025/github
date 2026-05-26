0976b71a94772ea07d80c83fa2ab0eaa5422bc3c
sumitmann2025 <sumitmann69d@gmail.com>
2026-05-26 23:20:08 +0530

    Add multiple resource groups in rg.tf

--- rg.tf
+++ rg.tf
@@ -6,4 +6,11 @@ resource "azurerm_resource_group" "rg1"{
   name = "rg_sumit008"
   location = "eastus"
   }
-
+resource "azurerm_resource_group" "rg1"{
+  name = "rg_sumit1111"
+  location = "eastus"
+  }
+resource "azurerm_resource_group" "rg1"{
+  name = "rg_sumit2222"
+  location = "eastus"
+  }
